bd225ee42682694f1f3cfc19491c02ab8a4dc6e3
Matthew Xi <deepwinter@winterroot.net>
2015-06-15 16:28:59 -0700

    Projection file for HRAP to generate even grid, and script for rasterizing this grid

---
+++ ToRaster.py
@@ -0,0 +1,25 @@
+import arcpy
+from arcpy import env
+import os
+from os import listdir
+
+env.workspace = "C:/Users/deepwinter/Documents/ArcGIS/Projects/NASQAN"
+precip_path = env.workspace + "/precip_2009_2015"
+
+print precip_path
+
+valField = "Globvalue"
+assignmentType = "SUM"
+priorityField = None
+cellSize = 1
+
+for f in listdir(precip_path):
+
+    fileName, fileExtension = os.path.splitext(f)
+    
+    #setup
+    inFeatures = env.workspace + "/precip_2009_2015/" + f
+    outRaster = env.workspace + "/precip_2009_2015_raster.gdb/" + fileName
+
+    #execute PointToRaster
+    arcpy.PointToRaster_conversion(inFeatures, valField, outRaster, assignmentType, priorityField, cellSize)
